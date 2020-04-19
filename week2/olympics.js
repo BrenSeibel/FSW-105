27e0fa4fba07ef9442f93675c963e56522afd37e
BrenSeibel <brens.seibel@gmail.com>
2020-04-19 15:32:31 -0400

    debugging

---
+++ week2/olympics.js
@@ -0,0 +1,20 @@
+
+if (5 > 3) {
+
+    console.log("is greater than")
+
+}
+var cat='cat'
+if(cat.length===3){
+    console.log("is the length")
+}
+var dog='dog'
+if(cat.length===dog.length) {console.log("the same")} else {
+    console.log("not the same")
+}
+
+
+
+
+
+console.log
