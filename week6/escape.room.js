a0d3e20bad855e09b691140df43cf33e61a8afdc
BrenSeibel <brens.seibel@gmail.com>
2020-05-18 00:08:25 -0400

    push

--- week6/escape.room.js
+++ week6/escape.room.js
@@ -1,6 +1,5 @@
 var readlineSync = require('readline-sync');
 
-//Read Player's Name
 var userName = readlineSync.question('Your name please? ');
 
 const strWelcome = `Welcome ${userName} to the Escape Room Simulation!`;
@@ -14,30 +13,25 @@ while(isPlayerAlive === true)
     
     if (myGameMenuOptions == 1)
     {
-        //User Dies
         console.log(`Sorry ${userName}! You are DEAD, Game OVER!`);
         isPlayerAlive = false;
     }
     else if (myGameMenuOptions == 2 && hasKey == false)
     {
-        //first time selecting option 2,player never had access to the key
         console.log(`${userName}, you have found the KEY!`);
         hasKey = true;
     }
     else if (myGameMenuOptions == 2 && hasKey == true)
     {
-        //Player selected option 2, player already had access to the key
         console.log(`${userName}, you had found the key earlier!`);
     }
     else if (myGameMenuOptions == 3 && hasKey == false)
     {
-        //first time selecting option 3, player does not have the key
         console.log(`${userName} - you need to find the key first!`);
         hasKey = true;
     }
     else if (myGameMenuOptions == 3 && hasKey == true)
     {
-        //player selected option 3, player does have the key
         console.log(`${userName}, you found have the key, and you opened the door, You WIN!`);
         isPlayerAlive = false;
     }
